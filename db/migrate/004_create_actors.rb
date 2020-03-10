b0fed1ff3d22da66af70f636962d0ce3eb0d9d6b
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-03-10 05:56:32 +0000

    Automatically backed up by Learn

---
+++ db/migrate/004_create_actors.rb
@@ -0,0 +1,9 @@
+class CreateActors < ActiveRecor::Migration
+  def change
+      create_table :actors do |t|
+        t.text :first_name
+        t.text :last_name 
+      end
+    end
+  end
+end 
---
+++ db/migrate/009_add_characters_to_actors.rb
@@ -0,0 +1,4 @@
+class AddCharactersToActors < ActiveRecord::Migration
+  def change
+    add_column :actors, :character_id, :integer
+  end
